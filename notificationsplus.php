8d99777f8b74ab401b039d6420eef2b1f02311d4
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2014-01-12 02:39:48 +0600

    initial commit

---
+++ notificationsplus.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Plugin Name: Notifications+
+Plugin URI: https://github.com/micc83/WpDevTool
+Description: A realtime notification plugin for WordPress forked from the Wp Heartbeat Notify project.
+Version: 1.0
+Author: Riyadh Al Nur
+Author URI: http://verticalaxisbd.com
+License: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+require_once( 'core/class-nplus-plugin.php' );
+require_once( 'core/class-nplus.php' );
+
+global $nplus_plugin;
+
+// Create the plugin and store it as a global
+$nplus_plugin = new NPlus_Plugin( 
+	__FILE__, 
+	array(
+		'required'	=>	array( 
+			'wordpress'	=>	'3.8' // WordPress 3.8 is required
+		)
+	) 
+);
+
+// Start notifications
+new notifications_plus( array(
+	'context'	=>	array( 'front' ),	// This plugin is supposed to work only on the front end
+	'base_url'	=>	$nplus_plugin->uri		// Set js and css base url
+) );
+
+// Hook for posts
+add_filter ( 'publish_post', 'notify_published_post' );
+function notify_published_post( $post_id ) {
+	
+	global $nplus_plugin;
+	
+	notifications_plus::notify( array(
+		'title'		=>		__( 'New Article', $nplus_plugin->textdomain ),
+		'content'	=>	 	__( 'There\'s a new post, why don\'t you give a look at', $nplus_plugin->textdomain ) .
+							' <a href="' . get_permalink( $post_id ) . '">' . get_the_title( $post_id ) . '</a>',
+		'type'		=>		'update'
+	) );
+	
+	return $post_id;	
+}
+
+// Hook for comments
+add_filter ( 'comment_post', 'notify_published_comment' );
+function notify_published_comment( $post_id ) {
+	
+	global $nplus_plugin;
+	
+	notifications_plus::notify( array(
+		'title'		=>		__( 'New Article', $nplus_plugin->textdomain ),
+		'content'	=>	 	__( 'There\'s a new post, why don\'t you give a look at', $nplus_plugin->textdomain ) .
+							' <a href="' . get_permalink( $post_id ) . '">' . get_the_title( $post_id ) . '</a>',
+		'type'		=>		'update'
+	) );
+	
+	return $post_id;	
+}
